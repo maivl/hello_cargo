2e36d578d78202d52711bd97f0840cdd7dc98047
maivl wu <wkf39988@gmail.com>
2023-12-05 02:23:03 +0000

    feat: update web

--- src/lib.rs
+++ src/lib.rs
@@ -2,17 +2,25 @@ use aes::cipher::{AsyncStreamCipher, KeyIvInit};
 use cfb_mode::Encryptor;
 use cfb_mode::Decryptor;
 use aes::Aes128;
+use arrayref::array_ref;
+use wasm_bindgen::prelude::*;
 
-pub fn encrypt(key: [u8; 16], iv: [u8; 16], plaintext: &[u8]) -> Vec<u8> {
+#[wasm_bindgen]
+pub fn encrypt(key: Vec<u8>, iv: Vec<u8>, plaintext: &[u8], memory: &mut [u8]) {
     type Aes128CfbEnc = Encryptor<Aes128>;
     let mut buf = plaintext.to_vec();
-    Aes128CfbEnc::new(&key.into(), &iv.into()).encrypt(&mut buf);
-    buf
+    let key_slice: &[u8; 16] = array_ref!(key, 0, 16);
+    let iv_slice: &[u8; 16] = array_ref!(iv, 0, 16);
+    Aes128CfbEnc::new(key_slice.into(), iv_slice.into()).encrypt(&mut buf);
+    memory.copy_from_slice(&buf);
 }
 
-pub fn decrypt(key: [u8; 16], iv: [u8; 16], ciphertext: &[u8]) -> Vec<u8> {
+#[wasm_bindgen]
+pub fn decrypt(key: Vec<u8>, iv: Vec<u8>, ciphertext: &[u8], memory: &mut [u8]) {
     type Aes128CfbDec = Decryptor<Aes128>;
     let mut buf = ciphertext.to_vec();
-    Aes128CfbDec::new(&key.into(), &iv.into()).decrypt(&mut buf);
-    buf
+    let key_slice: &[u8; 16] = array_ref!(key, 0, 16);
+    let iv_slice: &[u8; 16] = array_ref!(iv, 0, 16);
+    Aes128CfbDec::new(key_slice.into(), iv_slice.into()).decrypt(&mut buf);
+    memory.copy_from_slice(&buf);
 }
