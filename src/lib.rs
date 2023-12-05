ff64bce2d1e4e8a05cafdfef6eeb4469fe19feaf
maivl wu <wkf39988@gmail.com>
2023-12-05 01:49:44 +0000

    feat: v1

--- src/lib.rs
+++ src/lib.rs
@@ -1,38 +1,18 @@
 use aes::cipher::{AsyncStreamCipher, KeyIvInit};
-use hex_literal::hex;
+use cfb_mode::Encryptor;
+use cfb_mode::Decryptor;
+use aes::Aes128;
 
-type Aes128CfbEnc = cfb_mode::Encryptor<aes::Aes128>;
-type Aes128CfbDec = cfb_mode::Decryptor<aes::Aes128>;
-
-
-pub fn main () {
-
-    let key = [0x42; 16];
-    let iv = [0x24; 16];
-    let plaintext = *b"hello world! this is my plaintext.";
-    let ciphertext = hex!(
-        "3357121ebb5a29468bd861467596ce3d6f99e251cc2d9f0a598032ae386d0ab995b3"
-    );
-
-    // encrypt/decrypt in-place
+pub fn encrypt(key: [u8; 16], iv: [u8; 16], plaintext: &[u8]) -> Vec<u8> {
+    type Aes128CfbEnc = Encryptor<Aes128>;
     let mut buf = plaintext.to_vec();
     Aes128CfbEnc::new(&key.into(), &iv.into()).encrypt(&mut buf);
-    assert_eq!(buf[..], ciphertext[..]);
+    buf
+}
 
+pub fn decrypt(key: [u8; 16], iv: [u8; 16], ciphertext: &[u8]) -> Vec<u8> {
+    type Aes128CfbDec = Decryptor<Aes128>;
+    let mut buf = ciphertext.to_vec();
     Aes128CfbDec::new(&key.into(), &iv.into()).decrypt(&mut buf);
-    assert_eq!(buf[..], plaintext[..]);
-
-    // encrypt/decrypt from buffer to buffer
-    // buffer length must be equal to input length
-    let mut buf1 = [0u8; 34];
-    Aes128CfbEnc::new(&key.into(), &iv.into())
-        .encrypt_b2b(&plaintext, &mut buf1)
-        .unwrap();
-    assert_eq!(buf1[..], ciphertext[..]);
-
-    let mut buf2 = [0u8; 34];
-    Aes128CfbDec::new(&key.into(), &iv.into())
-        .decrypt_b2b(&buf1, &mut buf2)
-        .unwrap();
-    assert_eq!(buf2[..], plaintext[..]);
+    buf
 }
