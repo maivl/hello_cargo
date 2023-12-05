7bfe8c1fdb4b281a96fd26821c14f52cd1b8b531
maivl wu <wkf39988@gmail.com>
2023-12-05 03:03:44 +0000

    fet: update

--- src/lib.rs
+++ src/lib.rs
@@ -3,24 +3,23 @@ use cfb_mode::Encryptor;
 use cfb_mode::Decryptor;
 use aes::Aes128;
 use arrayref::array_ref;
-use wasm_bindgen::prelude::*;
 
-#[wasm_bindgen]
-pub fn encrypt(key: Vec<u8>, iv: Vec<u8>, plaintext: &[u8], memory: &mut [u8]) {
+#[no_mangle]
+pub fn encrypt(key: Vec<u8>, iv: Vec<u8>, plaintext: &[u8]) -> Vec<u8> {
     type Aes128CfbEnc = Encryptor<Aes128>;
     let mut buf = plaintext.to_vec();
     let key_slice: &[u8; 16] = array_ref!(key, 0, 16);
     let iv_slice: &[u8; 16] = array_ref!(iv, 0, 16);
     Aes128CfbEnc::new(key_slice.into(), iv_slice.into()).encrypt(&mut buf);
-    memory.copy_from_slice(&buf);
+    buf
 }
 
-#[wasm_bindgen]
-pub fn decrypt(key: Vec<u8>, iv: Vec<u8>, ciphertext: &[u8], memory: &mut [u8]) {
+#[no_mangle]
+pub fn decrypt(key: Vec<u8>, iv: Vec<u8>, ciphertext: &[u8]) -> Vec<u8> {
     type Aes128CfbDec = Decryptor<Aes128>;
     let mut buf = ciphertext.to_vec();
     let key_slice: &[u8; 16] = array_ref!(key, 0, 16);
     let iv_slice: &[u8; 16] = array_ref!(iv, 0, 16);
     Aes128CfbDec::new(key_slice.into(), iv_slice.into()).decrypt(&mut buf);
-    memory.copy_from_slice(&buf);
+    buf
 }
